4bb109d4eaef9f66b8553a32a5a43c655a89ec84
jeoldmixon <jeoldmixon@yahoo.com>
2020-06-01 22:35:47 -0500

    lesson 3-2-6

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,11 +3,7 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -90,6 +86,8 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
     // if no (false), ask question again by running fight() again
     else {
-        fight();
+        for (var i = 0; i < enemyNames.length; i++) {
+            fight(enemyNames[i]);
+        }
     }
 }
